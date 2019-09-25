0025855a0e1165c377296b8a480387b4635f017c
Max Alyokhin <alexmaxvik@yandex.ru>
2019-09-25 23:10:27 +0300

    Обновление от 25.09
    - обновлён guplfile.js
    - фикс бага с выравниванием текста в Cyclic Derive Screen

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
 'use strict';
 
-const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const imageminPngquant = require('imagemin-pngquant');
-const rigger = require('gulp-rigger');
-const rimraf = require('rimraf');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
+const { src, dest, watch, series, parallel } = require('gulp'),
+                            sass             = require('gulp-sass'), // Sass компиллятор
+                            sourcemaps       = require('gulp-sourcemaps'), // Sourcemaps
+                            autoprefixer     = require('gulp-autoprefixer'), // Автопрефиксер CSS
+                            uglify           = require('gulp-uglify'), // Минификация js
+                            imagemin         = require('gulp-imagemin'), // Компрессия изображений
+                            imageminPngquant = require('imagemin-pngquant'), // Дополнение для .png
+                            rigger           = require('gulp-rigger'), //Подключение шаблонов
+                            rimraf           = require('rimraf'), // Удаление файлов
+                            order            = require("gulp-order"), // Задаёт порядок конкатенации
+                            concat           = require('gulp-concat'), // Конкатенация
+                            babel            = require('gulp-babel'); // Транспайлер в ES5
+                            browserSync      = require('browser-sync'), // Сервер
+                            reload           = browserSync.reload;
 
 //Здесь хранятся пути до всех файлов
-var path = {
+const path = {
     //Брать исходники здесь:
     src: { 
         html:  'src/html/**/*.html',
         sass:  'src/sass/**/*.sass',
-        js:    'src/js/**/*.js',
+        js:    'src/js/**/*.js', 
         img:   'src/images/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        fonts: 'src/fonts/**/*.*',
+        libs_js:  'src/libs/**/*.js',
+        libs_sass:  'src/libs/**/*.sass'
     },
     //За изменением каких файлов мы хотим наблюдать:
     watch: { 
@@ -42,7 +47,7 @@ var path = {
 };
 
 //Здесь настройки сервера
-var config = {
+const config = {
     server: {
         baseDir: "dist"
     },
@@ -54,7 +59,7 @@ var config = {
 };
 
 //Сборка html
-function html_build() {
+function html() {
     return src(path.src.html) //Путь до исходных файлов в src
         .pipe(rigger()) //Rigger позволяет использовать шаблоны и подключать их в документы
         .pipe(dest(path.dist.html)) //Вывод готового в dist
@@ -62,48 +67,45 @@ function html_build() {
 }
 
 //Сборка css
-function style_min_build() {
-    return src(path.src.sass) //Путь до исходных файлов в src
-        .pipe(sourcemaps.init()) //Инициализируем sourcemaps
+function style() {
+    return src([path.src.sass, path.src.libs_sass]) //Путь до исходных файлов в src
+        // .pipe(sourcemaps.init()) //Инициализируем sourcemaps
         .pipe(sass({ //Параметры gulp-sass
-            sourceMap: true, //sourcemaps включены
+            sourceMap: false, //sourcemaps выключены
             errLogToConsole: true, //Пишем логи
             outputStyle: 'compressed' //Минифицируем
         }))
-        .pipe(autoprefixer()) //Добавляем вендорные префиксы, настраивается через package.json в browserlist
-        .pipe(sourcemaps.write()) //Прописываем sourcemaps
-        .pipe(dest(path.dist.css)) //Вывод готового в dist
-        .pipe(reload({stream: true})); //Обновляем сервер
-}
-
-//Сборка css без минификации
-function style_build() {
-    return src(path.src.sass) //Путь до исходных файлов в src
-        .pipe(sourcemaps.init()) //Инициализируем sourcemaps
-        .pipe(sass({ //Параметры gulp-sass
-            sourceMap: true, //sourcemaps включены
-            errLogToConsole: true, //Пишем логи
-            
-        }))
-        .pipe(autoprefixer()) //Добавляем вендорные префиксы, настраивается через package.json в browserlist
-        .pipe(sourcemaps.write()) //Прописываем sourcemaps
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserslist: ['last 10 versions']})) //Добавляем вендорные префиксы, настраивается также через package.json в browserlist
+        // .pipe(sourcemaps.write()) //Прописываем sourcemaps
         .pipe(dest(path.dist.css)) //Вывод готового в dist
         .pipe(reload({stream: true})); //Обновляем сервер
 }
 
 //Сборка js
-function js_build() {
-    return src(path.src.js) //Путь до исходных файлов в src
+function js() {
+    return src([path.src.js, path.src.libs_js]) //Путь до исходных файлов в src
         .pipe(rigger()) //Rigger позволяет использовать шаблоны и подключать их в документы
-        .pipe(sourcemaps.init()) //Инициализируем sourcemaps
+        // .pipe(sourcemaps.init()) //Инициализируем sourcemaps
+        .pipe(babel({ // Запускаем Babel
+			presets: ['@babel/preset-env']
+		}))
+        .pipe(order([
+            path.src.libs_js,
+
+            path.src.js
+          ]))
+        .pipe(concat('main.min.js'))
         .pipe(uglify()) //Минифицируем js
-        .pipe(sourcemaps.write()) //Пишем логи
+        // .pipe(sourcemaps.write()) //Пишем sourcemaps
         .pipe(dest(path.dist.js)) //Вывод готового в dist
         .pipe(reload({stream: true})); //Обновляем сервер
 }
 
 //Оптимизация изображений
-function image_build() {
+function image() {
     return src(path.src.img) //Путь до исходных файлов в src
         .pipe(imagemin({plugins: [imageminPngquant()]})) //Оптимизация изображений + плагин для png
         .pipe(dest(path.dist.img)) //Вывод готового в dist
@@ -111,7 +113,7 @@ function image_build() {
 }
 
 //Шрифты (перенос из src в dist)
-function fonts_build() {
+function fonts() {
     return src(path.src.fonts) //Вход
         .pipe(dest(path.dist.fonts)) //Выход
         .pipe(reload({stream: true})); //Обновляем сервер
@@ -127,28 +129,31 @@ function clean(cb) {
 //Удалить dist
 exports.clean = clean;
 
-//Собрать Sass без минификации
-exports.sass = style_build;
+//Собрать Sass
+exports.sass = style;
+
+//Собрать js
+exports.js = js;
 
 //Собрать проект
-exports.build = series(clean, html_build, style_min_build, js_build, image_build, fonts_build);
+exports.build = series(clean, html, style, js, image, fonts);
 
 //Запуск сервера
 exports.dev = function() {
     browserSync(config)
-    watch(path.src.html, html_build)
-    watch(path.src.sass, style_min_build)
-    watch(path.src.js, js_build)
-    watch(path.src.img, image_build)
-    watch(path.src.fonts, fonts_build)
+    watch(path.src.html, html)
+    watch(path.src.sass, style)
+    watch(path.src.js, js)
+    watch(path.src.img, image)
+    watch(path.src.fonts, fonts)
 }
 
 //По дефолту всё собираем и запускаем сервер
-exports.default = parallel(html_build, style_min_build, js_build, image_build, fonts_build, function() {
+exports.default = parallel(html, style, js, image, fonts, function() {
     browserSync(config)
-    watch(path.src.html, html_build)
-    watch(path.src.sass, style_min_build)
-    watch(path.src.js, js_build)
-    watch(path.src.img, image_build)
-    watch(path.src.fonts, fonts_build)
+    watch(path.src.html, html)
+    watch(path.src.sass, style)
+    watch(path.src.js, js)
+    watch(path.src.img, image)
+    watch(path.src.fonts, fonts)
 })
